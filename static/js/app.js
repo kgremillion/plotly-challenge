6116eaef6bc0ff9d739712486965d703dfa1cba0
kgremillion <kgremillion19@gmail.com>
2020-05-27 23:31:14 -0500

    Added charts

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,9 @@ function optionChanged(nameSelect) {
   updateMeta(nameSelect);
   createCharts(nameSelect)
 }
-// This function is called when a dropdown menu item is selected
+/** This function is called when a dropdown menu item is selected
+ * @param selection - value user selected from dropdown box 
+*/ 
 function updateMeta(selection) {
 
   // May need to delete due to possible loading of data twice
@@ -44,56 +46,70 @@ function createCharts(chartSelect) {
   // Use D3 fetch to read the JSON file
   // The data from the JSON file is arbitrarily named importedData as the argument
   d3.json("data/samples.json").then(function (data) {
-    console.log("chart", chartSelect);
+    console.log("data", data);
 
-    var chart_values = data.sample_values;
+    var chart_values = data.samples;
+    console.log("chart_values", chart_values);
+
     var selectionSample = chart_values.filter(object => object.id == chartSelect);
-    var otu_ids = data.otu_ids;
-    var otu_labels = data.otu_labels;
-    console.log("sample_values", sample_values);
+    console.log("selectionSample", selectionSample[0]);
+    var sample_values = selectionSample[0].sample_values;
+    console.log("sample_values", sample_values)
+    var otu_ids = selectionSample[0].otu_ids;
+    var otu_labels = selectionSample[0].otu_labels;
+    console.log("otu_ids", otu_ids);
+    console.log("otu_labels", otu_labels);
 
-    // let bubbleLayout = {
-    //   margin: { t: 0 },
-    //   hovermode: "closests",
-    //   xaxis: { title: "OTU ID"}
-    // }
-
-    // let bubbleData = [
-    //   {
-    //     x: otu_ids,
-    //     y: sample_values,
-    //     text: otu_labels,
-    //     mode: "markers",
-    //     marker: {
-    //       size: sample_values,
-    //       color: otu_ids,
-    //     }
-    //   }
-    // ]
-
-    // Plotly.plot("bubble", bubbleData, bubbleLayout);
 
     // // Create variables for top 10 data points
-    // var sample_values10 = sample_values10.slice(0, 10).reverse();
-    // var otu_ids10 = otu_ids10.slice(0, 10).reverse();
-    // var otu_labels10 = otu_labels10.slice(0, 10).reverse();
-    // console.log("samples_values10", sample_values10);
+    var sample_values10 = sample_values.slice(0, 10);
+    console.log("samples_values10", sample_values10);
+    var otu_ids10 = otu_ids.slice(0, 10);
+    console.log("otu_ids10", otu_ids10);
+    var otu_labels10 = otu_labels.slice(0, 10);
+    console.log("otu_labels10", otu_labels10);
+
+    // Create bubble chart layout for all data
+    var bubbleChart = {
+      margin: { t: 0 },
+      xaxis: { title: "OTU ID"}    
+    }
+
+    // Refer to all data to plot all points on bubbles chart
+    var bubbleData = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      // Make the marker color & size reflect the amount of bacteria 
+      marker: {
+        color: otu_ids,
+        size: sample_values
+      },
+      type: 'scatter'
+    };
+
+    Plotly.plot("bubble", [bubbleData], bubbleChart);
+
+    // Create bar chart layout for top ten data points in the selected sample
+    var barChart = {
+      title: "Top Bacteria",
+      barmode: "group"
+    };
+    
+    var barData = {
+      x: sample_values10,
+      y: otu_ids10,
+      orientation: 'h',
+      text: otu_ids10.map(String),
+      hoverinfo: otu_labels,
+      type: "bar"
+    };
+      
+    Plotly.newPlot("bar", [barData], barChart);
+
   });
 }
 
-  // Assign the value of the dropdown menu option to a variable
-  // var dataset = dropdownMenu.property("value");
-
-
-
-//   // Trace1 for the Greek Data
-//   var trace1 = {
-//     x: data.map(row => row.greekSearchResults),
-//     y: data.map(row => row.greekName),
-//     text: data.map(row => row.greekName),
-//     name: "Greek",
-//     type: "bar",
-//     orientation: "h"
-//   };
 
 init();
